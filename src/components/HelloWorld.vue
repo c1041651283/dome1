cd49d9fa78f84d1741dbcd44a4ad3b11fb81a8a9
chengguanwen <m18322003462@163.com>
2020-11-16 01:14:58 +0800

    测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -40,7 +40,7 @@ const data = [
   { year: '1993', value: 3.5 },
   { year: '1994', value: 5 },
   { year: '1995', value: 4.9 },
-  { year: '1996', value: 6 },
+  { year: '1996', value: 7 },
   { year: '1997', value: 7 },
   { year: '1998', value: 9 },
   { year: '1999', value: 13 },
