27fcf4f9a7861a05fc033d8ddf205c1779a8b42e
chengguanwen <m18322003462@163.com>
2020-11-23 22:25:33 +0800

    折线图与当前数据表的数据联动。

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,123 +1,125 @@
 <template>
   <div class="page">
     <div>
-      <a-table :columns="columns" :data-source="data">
+      <a-table
+          :columns="columns"
+          :data-source="data"
+          :showHeader="showHeader"
+          :pagination="pagination"
+          bordered
+      >
         <template slot="title">
-          <div class="right-title">
-            列表
+          <div class="right-title">ユーザー情報</div>
+          <div class="left-title">
+            <span v-if="flag">
+              <a @click="() => edit()">变更</a>
+            </span>
+            <span v-else>
+              <a @click="() => save()">保存</a>
+              <a @confirm="() => cancel()">取消</a>
+            </span>
           </div>
-          <div style="float: right">
-            <a @click="showModal">变更</a>
+        </template>
+        <template v-for="col in ['content']" :slot="col" slot-scope="text, record">
+          <div :key="col">
+            <template v-if="flag">
+              {{ text }}
+            </template>
+            <a-input
+                v-else
+                style="margin: -5px 0"
+                :value="text"
+                @change="(e) => handleChange(e.target.value, record.key, col)"
+            />
           </div>
         </template>
       </a-table>
     </div>
     <div>
-      <a-modal
-          title="Title"
-          :visible="visible"
-          :confirm-loading="confirmLoading"
-          :centered = "centered"
-          @ok="handleOk"
-          @cancel="handleCancel"
-      >
-        <a-form-model layout='vertical' :form="form">
-          <a-form-model-item label="用户名">
-            <a-input/>
-          </a-form-model-item>
-          <a-form-model-item label="用户名">
-            <a-input/>
-          </a-form-model-item>
-          <a-form-model-item label="用户名">
-            <a-input/>
-          </a-form-model-item>
-          <a-form-model-item label="用户名">
-            <a-input/>
-          </a-form-model-item>
-          <a-form-model-item label="用户名">
-            <a-input/>
-          </a-form-model-item>
-          <a-form-model-item label="用户名">
-            <a-input/>
-          </a-form-model-item>
-        </a-form-model>
-      </a-modal>
-    </div>
-    <div>
-      <a>修改密码</a>
+      <a>パスワードを変更></a>
     </div>
   </div>
-
 </template>
 <script>
 const columns = [
   {
-    title: 'Name',
-    dataIndex: 'name',
+    title: "info",
+    dataIndex: "info",
+    width: "150px",
+    scopedSlots: { customRender: "info" },
   },
   {
-    title: 'Cash Assets',
-    dataIndex: 'money',
+    title: "content",
+    dataIndex: "content",
+    width: "150px",
+    scopedSlots: { customRender: "content" },
   },
 ];
-
-const data = [
-  {
-    key: '1',
-    name: 'Jim Green',
-    money: 'John Brown',
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    money: '￥1,256,000.00',
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    money: '￥120,000.00',
-  },
-];
-
+const data = [];
+for (let i = 0; i < 5; i++) {
+  data.push({
+    key: i.toString(),
+    info: `Edrward ${i}`,
+    content: 32,
+  });
+}
 export default {
-  beforeCreate() {
-    //创建表单
-    this.form = this.$form.createForm(this, { name: 'form_in_modal' });
-  },
   data() {
+    this.cacheData = data.map((item) => ({ ...item }));
     return {
-      centered:true,
+      showHeader: false,
+      flag: true,
       data,
       columns,
-      visible: false,
-      confirmLoading: false,
+      pagination: false,
     };
   },
-  methods:{
-    showModal() {
-      this.visible = true;
+  methods: {
+    handleChange(value, key, column) {
+      const newData = [...this.data];
+      const target = newData.filter((item) => key === item.key)[0];
+      if (target) {
+        target[column] = value;
+        this.data = newData;
+      }
     },
-    handleOk() {
-      const form = this.form;
-      window.console.log('form 表单内容: ', form.value);
-      form.resetFields();
-      this.visible = false;
+    edit() {
+      const newData = [...this.data];
+      this.data = newData;
+      this.flag = false;
     },
-    handleCancel() {
-      console.log('Clicked cancel button');
-      this.visible = false;
+    save() {
+      const newData = [...this.data];
+      const newCacheData = [...this.cacheData];
+      this.data = newData;
+      this.flag = true;
+      this.cacheData = newCacheData;
     },
-  }
+    cancel(key) {
+      const newData = [...this.data];
+      const target = newData.filter((item) => key === item.key)[0];
+      if (target) {
+        Object.assign(
+            target,
+            this.cacheData.filter((item) => key === item.key)[0]
+        );
+        delete target.editable;
+        this.data = newData;
+      }
+      this.flag = true;
+    },
+  },
 };
 </script>
 <style>
-.page{
+.page {
   width: 800px;
   margin: 0 auto;
 }
-.right-title{
-  float: left;
-  margin-left: 20px;
-  font-size: 20px;
-}
+/*.right-title {*/
+/*  float: right;*/
+/*}*/
+/*.left-title {*/
+/*  float: left;*/
+/*}*/
 </style>
